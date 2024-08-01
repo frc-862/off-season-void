3f4ece7e9b8a7809b377d0115e9bd7306ab7d175
Meh243 <120067923+Meh243@users.noreply.github.com>
2024-07-31 20:27:20 -0400

    [#16] rookies got the job done

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,34 +6,21 @@ import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
 
-    // TODO: create a TalonFX variable named "shooterMotor"
-
     private TalonFX shooterMotor;
-    /*
-     * REMEMBER: use the "private" keywords
-     * (you don't want this "shooterMotor" variable to be used outside the subsystem)
-     * (you don't want this "shooterMotor" variable to be changed)
-     */
 
     
     /** Creates a new Shooter Subsystem. */
     public Shooter() {
         shooterMotor = new TalonFX(ShooterConstants.SHOOTER_MOTOR_CAN);
-        // TODO: set a TalonFX object to the newly created shooter motor while using the CAN ID from "ShooterConstants"
-        // REMEMBER: use "new" when creating an object from a class
     }
 
-    // TODO: create the method "setPower" with the double type "power" as a parameter
-    /*
-     * REMEMBER: you want to use the "public" keyword
-     * (you want to use the method outside of the class)
-     */
+    public void setPower(double power) {
+        shooterMotor.set(power);
+    }
 
-    // TODO: create the method "stop" which calls the "setPower" method with the "power" parameter set to zero
-    /*
-     * REMEMBER: you want to use the "public" keyword
-     * (you want to use the method outside of the class)
-     */
+    public void stop() {
+        setPower(0);
+    }
 
     @Override
     public void periodic() {}
